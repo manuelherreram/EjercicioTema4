e671ad5b37e282b7f656cee6470a1aa034c34e7d
Manuel Herrera <manuel.herrera.m@gmail.com>
2022-08-05 15:45:42 -0400

    Add switch example

--- Tarea4/src/Main.java
+++ Tarea4/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args)  {
-        int numeroIf = -1;
+        // Primer ejercicio
+        int numeroIf = 4;
         if (numeroIf >= 1){
             System.out.println("Es positivo");
         } else if (numeroIf < 0){
@@ -8,6 +9,48 @@ public class Main {
         } else {
             System.out.println("Es 0");
         }
+
+        // Segundo ejercicio
+        int numeroWhile = 1;
+        while(numeroWhile <3){
+            System.out.println(numeroWhile);
+            numeroWhile = numeroWhile +1;
+        }
+
+        // Tercer ejercicio
+        int numeroDoWhile = 2;
+        do {
+            System.out.println(numeroDoWhile);
+            numeroDoWhile = numeroDoWhile +1;
+        }
+        while(numeroWhile <3);
+
+        // Cuarto ejercicio
+        int numeroFor = 0;
+        for(int i=3; i <= numeroFor; i++){
+            System.out.println(numeroFor);
+        }
+
+        // Quinto ejercicio
+        var estacion = "Verano";
+
+        switch(estacion){
+            case "Invierno":
+                System.out.println("Es invierno");
+                break;
+            case "Otoño":
+                System.out.println("Es otoño");
+                break;
+            case "Primavera":
+                System.out.println("Es primavera");
+                break;
+            case "Verano":
+                System.out.println("Es verano");
+                break;
+            default:
+                System.out.println("No es una estación");
+        }
+        
 
     }
 }
